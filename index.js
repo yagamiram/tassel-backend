d44976f278f18f466f7eb381324d048408552926
Sri TK <sriram.ssnit@gmail.com>
2019-12-09 22:18:48 -0800

    POST call added; Write to CSV file added

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express'),
+    fs = require('fs')
+    url = require('url');
+var bodyParser = require('body-parser');
+var csvWriter = require('csv-write-stream')
+
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+var writer = csvWriter({ headers: ["userName", "userEmail", "phoneNum"]})
+writer.pipe(fs.createWriteStream('userInfo.csv'))
+writer.write([])
+writer.end()
+
+app.post('/saveUserInfo', function(req, res) {
+    var userName = req.body.userName;
+    var userEmail = req.body.userEmail;
+    var phoneNum = req.body.phoneNum;
+    const writer = csvWriter({sendHeaders: false})
+    writer.pipe(fs.createWriteStream('userInfo.csv', {flags: 'a'}))
+    writer.write({userName, userEmail, phoneNum})
+    writer.end();
+    res.send('Received successfully')
+})
+
+
+
+app.listen(8080, () => {
+    console.log(`Listening on port ${8080}`)
+})
